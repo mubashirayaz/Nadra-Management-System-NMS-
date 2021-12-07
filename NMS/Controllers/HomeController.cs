65ab5578e8050d54a621e1558fdd6a2c0b02fd7f
DESKTOP-GTJALHK\Mubashir <mubashirayaz123@gmail.com>
2021-12-07 10:24:23 +0500

    DashBoard and login Created

---
+++ NMS/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using NMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(login_tbl objuser)
+        {
+            NadraDbContext obj = new NadraDbContext();
+            var user = obj.login_tbl.Where(x => x.login_email == objuser.login_email && x.login_password == objuser.login_password);
+           
+            if (user != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ NMS/Controllers/login_tblController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NMS.Models;
+
+namespace NMS.Controllers
+{
+    public class login_tblController : Controller
+    {
+        private NadraDbContext db = new NadraDbContext();
+
+        // GET: login_tbl
+        public ActionResult Index()
+        {
+            var login_tbl = db.login_tbl.Include(l => l.staff_tbl);
+            return View(login_tbl.ToList());
+        }
+
+        // GET: login_tbl/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            login_tbl login_tbl = db.login_tbl.Find(id);
+            if (login_tbl == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login_tbl);
+        }
+
+        // GET: login_tbl/Create
+        public ActionResult Create()
+        {
+            ViewBag.staff_id = new SelectList(db.staff_tbl, "staff_id", "designation");
+            return View();
+        }
+
+        // POST: login_tbl/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "login_id,login_email,login_password,staff_id")] login_tbl login_tbl)
+        {
+            if (ModelState.IsValid)
+            {
+                db.login_tbl.Add(login_tbl);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.staff_id = new SelectList(db.staff_tbl, "staff_id", "designation", login_tbl.staff_id);
+            return View(login_tbl);
+        }
+
+        // GET: login_tbl/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            login_tbl login_tbl = db.login_tbl.Find(id);
+            if (login_tbl == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.staff_id = new SelectList(db.staff_tbl, "staff_id", "designation", login_tbl.staff_id);
+            return View(login_tbl);
+        }
+
+        // POST: login_tbl/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "login_id,login_email,login_password,staff_id")] login_tbl login_tbl)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(login_tbl).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.staff_id = new SelectList(db.staff_tbl, "staff_id", "designation", login_tbl.staff_id);
+            return View(login_tbl);
+        }
+
+        // GET: login_tbl/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            login_tbl login_tbl = db.login_tbl.Find(id);
+            if (login_tbl == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login_tbl);
+        }
+
+        // POST: login_tbl/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            login_tbl login_tbl = db.login_tbl.Find(id);
+            db.login_tbl.Remove(login_tbl);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
